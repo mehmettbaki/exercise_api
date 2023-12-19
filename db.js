0cca67adf74b65d66d99f69b54184ef209f0bcf3
mehmettbaki <mehmetbaki123@gmail.com>
2023-12-19 18:30:03 +0300

    modify app.listen

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const connectdb = async () => {
     try {
         await mongoose.connect(`mongodb+srv://usermehmet:${process.env.MONGO_PASSWORD}@dbname.bamd1su.mongodb.net/?retryWrites=true&w=majority`);
         console.log('mongodb bağlandık')
-        
+
     } catch (error) {
         console.log(error)
         console.log('db connection has been failed.')
